fb4ef96ec985a359300fdc42e502d70bb99d91ac
IDashM <148833447+IDashM@users.noreply.github.com>
2024-08-13 12:11:18 +0000

    hehe

--- New folder/lists.py
+++ New folder/lists.py
@@ -4,5 +4,14 @@ import random
 import threading
 import time
 
-l1 = {1,3.14,"DEREK", True}
+#      0   1    2       3
+l1 = {1, 3.14, "DEREK",True}
+print("Lenght", len(l1)) # gets the number of arrays 
+print("1st ", list(l1)[0])
+print("Last ", list(l1)[-1])
+list(l1)[0] = 2
+list(l1)[2:4] = ["Bob",False]
+list(l1)[2:2] = ["Paul", 9]
 
+
+print(l1)
