11dc2ca11d01f2961cabc9b3df0fbbb2b4e23a7f
IDashM <148833447+IDashM@users.noreply.github.com>
2024-08-14 13:25:24 +0000

    forloops unfinished

---
+++ forloops.py
@@ -0,0 +1,15 @@
+Lists = [1, 3.24, "Famador"]
+
+for x in Lists:
+    print(x)
+
+for x in [2,4,20,30,50]:
+    print(x)
+
+numlist = [[1,2,3],[10,20,30],[100,200,300]] #
+for x in range(0,3):
+    for y in range(0,3):
+        print(numlist[x][y])
+
+
+
